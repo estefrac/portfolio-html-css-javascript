3564a7767c23bf8fb4db1bc61766fdc9c97762da
Esteban Fraccascia <estefrac@gmail.com>
2025-09-05 18:15:17 -0300

    saludo animado

--- js/functions/saludarUsuario.js
+++ js/functions/saludarUsuario.js
@@ -1,6 +1,17 @@
 export function saludarUsuario(nombreUsuario, saludoUsuario){
   if (nombreUsuario){
-    saludoUsuario.textContent = `Hola ${nombreUsuario}! 👋`
-    saludoUsuario.classList.add('mostrar')
+  saludoUsuario.innerHTML = `
+    Hola ${nombreUsuario}! <span id="lottie-hand" style="width:25px; height:25px; display:inline-block; vertical-align:top; margin-top:-2px;"></span>
+  `;
+    saludoUsuario.classList.add('mostrar');
+    if (window.lottie) {
+      lottie.loadAnimation({
+        container: document.getElementById('lottie-hand'),
+        renderer: 'svg',
+        loop: true,
+        autoplay: true,
+        path: 'assets/4Z9SNfrWd7.json'
+      });
+    }
   }
 }
